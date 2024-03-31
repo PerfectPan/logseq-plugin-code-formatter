65b1ab52ce8ed3661319460b0106383e19314c42
Marcio Coutinho <marciohbc@gmail.com>
2024-03-31 13:30:03 -0300

    Adds support for JSON and Markdown (resolve #2)

--- src/language-map.ts
+++ src/language-map.ts
@@ -1,9 +1,9 @@
 import parserBabel from 'prettier/parser-babel';
 import parserTypeScript from 'prettier/parser-typescript';
 import parserHTML from 'prettier/parser-html';
-import parserCSS from 'prettier/parser-postcss';
+import parserMarkdown from 'prettier/parser-markdown';
 
-export type SupportedLanguage = 'js' | 'javascript' | 'ts' | 'typescript' | 'html' | 'css' | 'less' | 'scss';
+export type SupportedLanguage = 'js' | 'json' | 'javascript' | 'ts' | 'typescript' | 'html' | 'css' | 'less' | 'scss' | 'markdown';
 
 interface IPrettierOption {
   parser: string;
@@ -13,6 +13,10 @@ interface IPrettierOption {
 export const languageMap: Record<SupportedLanguage, IPrettierOption> = {
   js: {
     parser: 'babel',
+    plugins: [parserBabel]
+  },
+  json: {
+    parser: 'json',
     plugins: [parserBabel]
   },
   javascript: {
@@ -42,5 +46,10 @@ export const languageMap: Record<SupportedLanguage, IPrettierOption> = {
   scss: {
     parser: 'css',
     plugins: [parserCSS]
+  },
+  ,
+  markdown: {
+    parser: 'markdown',
+    plugins: [parserMarkdown]
   },
 }
